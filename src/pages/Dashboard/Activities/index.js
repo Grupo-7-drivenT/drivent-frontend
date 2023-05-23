2e9bb118f1261faf5fa39e86de8f20ae3c8f8a12
IzaFreire <kariny.frei.2000@gmail.com>
2023-05-23 11:17:52 -0300

    feat: Notifications (unpaid tickets & no activity selection)

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,83 @@
+import useTickets from '../../../hooks/api/useTicket';
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
+
 export default function Activities() {
-  return 'Atividades: Em breve!';
+  const [userTicket, setUserTicket] = useState(null);
+  const { getTicket } = useTickets();
+
+  useEffect(() => {
+    async function fetchUserTicket() {
+      try {
+        const ticket = await getTicket();
+        setUserTicket(ticket);
+      } catch (error) {
+        console.error('Erro ao obter informações do ingresso:', error);
+      }
+    }
+
+    fetchUserTicket();
+  }, []);
+
+  if (!userTicket) {
+    return (
+      <>
+        <TitleTypography variant="h4">Escolha de atividades</TitleTypography>
+        <CustomCenter>Nenhum ingresso encontrado.</CustomCenter>
+      </>
+    );
+  }
+
+  if (!userTicket.TicketType.includesHotel) {
+    return (
+      <>
+        <TitleTypography variant="h4">Escolha de atividades</TitleTypography>
+        <CustomCenter>
+          Sua modalidade de ingresso não necessita escolher atividade.
+          <br />Você terá acesso a todas as atividades
+        </CustomCenter>
+      </>
+    );
+  }
+
+  if (userTicket.status === 'PAID') {
+    return (
+      <>
+        <TitleTypography variant="h4">Escolha de atividades</TitleTypography>
+
+        <SecondTitle>Primeiro, filtre pelo dia do evento: </SecondTitle>
+
+      </>
+    );
+  }
+
+  return (
+    <CustomCenter>
+      Seu pagamento ainda não foi confirmado
+      <br />Não é possível reservar um hotel
+    </CustomCenter>
+  );
 }
+
+const TitleTypography = styled(Typography)`
+  margin-bottom: 20px !important;
+`;
+
+const CustomCenter = styled.span`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #8e8e8e;
+  font-size: 20px;
+  text-align: center;
+`;
+
+const SecondTitle = styled.div`
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 23px;
+  color: #8E8E8E;
+`;
